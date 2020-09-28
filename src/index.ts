ff4b1fcd2086888882778cf88a9f82d7ccee2ea1
Pavel Shvarchov <Shvarpa@gmail.com>
2020-09-28 14:43:32 +0300

    #fixed track resume; #recreating PeerConnection on each new SDP from ws:  *this is done because peer connection fails to adapt to new sdp.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import "webrtc-adapter";
 let ws: WebSocket;
 let pc: RTCPeerConnection;
 
-const createWsURL = (wsHost: string, wsPort: number, wsPath?: string) => `ws://${wsHost}${wsPort ? `:${wsPort}` : ""}/${wsPath}`;
-
 const send = (ws: WebSocket, data: Object) => ws.send(JSON.stringify(data));
 
 function playStream(videoElement: HTMLVideoElement, wsUrl: string, config: RTCConfiguration, reportError: (err: Error) => void = (err: Error) => console.error(err.message)) {
@@ -14,9 +12,17 @@ function playStream(videoElement: HTMLVideoElement, wsUrl: string, config: RTCCo
 	if (ws && ws.readyState == WebSocket.OPEN) ws.close();
 	ws = new WebSocket(wsUrl);
 
-	if (pc) pc.close();
-	pc = new RTCPeerConnection(config);
 	const onsdp = async (sdp: RTCSessionDescriptionInit) => {
+		if (pc) pc.close();
+		pc = new RTCPeerConnection(config);
+		pc.addEventListener("track", ({ track }) => {
+			mediaStream.addTrack(track);
+			track.addEventListener("unmute", () => mediaStream.addTrack(track));
+			track.addEventListener("mute", () => mediaStream.removeTrack(track));
+		});
+		pc.addEventListener("icecandidate", ({ candidate: data }) => {
+			if (data) send(ws, { type: "ice", data });
+		});
 		try {
 			await pc.setRemoteDescription(sdp);
 			let data = await pc.createAnswer();
@@ -33,16 +39,6 @@ function playStream(videoElement: HTMLVideoElement, wsUrl: string, config: RTCCo
 			if (reportError) reportError(err);
 		}
 	};
-
-	pc.addEventListener("track", ({ track }) => {
-		mediaStream.addTrack(track);
-		track.addEventListener("mute", () => {
-			mediaStream.removeTrack(track);
-		});
-	});
-	pc.addEventListener("icecandidate", ({ candidate: data }) => {
-		if (data) send(ws, { type: "ice", data });
-	});
 	ws.addEventListener("message", (ev) => {
 		try {
 			let { type, data } = JSON.parse(ev.data);
@@ -61,4 +57,5 @@ window.onload = () => {
 	videoElement.autoplay = true;
 	videoElement.muted = true;
 	playStream(videoElement, `ws://${window.location.host}/ws`, config);
+	// playStream(videoElement, `ws://localhost:1234/ws`, config);
 };
